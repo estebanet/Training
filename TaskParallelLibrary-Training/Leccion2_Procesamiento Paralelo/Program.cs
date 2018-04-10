fdd282dbb3e8235013e75b2007456082a0506300
estebanet <38247804+estebanet@users.noreply.github.com>
2018-04-10 18:53:23 -0500

    EGR 2018/04/10 - Se añade proyecto correspondiente a la lección 2 Procesamiento paralelo.

---
+++ TaskParallelLibrary-Training/Leccion2_Procesamiento Paralelo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Leccion2_Procesamiento_Paralelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunParallelTasks();
+            Console.WriteLine("Presione <enter> para finalizar");
+            Console.ReadLine();
+
+            //Continuar Ejercicio 1 - Tarea 3 / Ejecución de ciclos en paralelo.
+        }
+
+        static void RunParallelTasks()
+        {
+            Console.WriteLine("Iniciando ejecución de multiples tareas...");
+            Parallel.Invoke(
+                () => WriteToConsole("Tarea 1"),
+                () => WriteToConsole("Tarea 2"),
+                () => WriteToConsole("Tarea 3"));
+        }
+
+        static void WriteToConsole(string message)
+        {
+            int ThreadId = Thread.CurrentThread.ManagedThreadId;
+
+            Console.WriteLine($"{message}, Thread: {ThreadId}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Fin de la tarea {message}, Thread: {ThreadId}");
+        }
+    }
+}
