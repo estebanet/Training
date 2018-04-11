e440323b6598542b85e30c2f26d36039d1197805
estebanet <38247804+estebanet@users.noreply.github.com>
2018-04-11 09:21:26 -0500

    Se completan tareas del ejercicio 1 "Ejecutando un conjunto de tareas de forma simultánea"

---
+++ TaskParallelLibrary-Training/Leccion2_Procesamiento Paralelo/ProductDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leccion2_Procesamiento_Paralelo
+{
+    class ProductDTO
+    {
+        public ProductDTO()
+        {
+            System.Threading.Thread.Sleep(1);
+        }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal? ProductPrice { get; set; }
+        public decimal? UnitsInStock { get; set; }
+    }
+}
