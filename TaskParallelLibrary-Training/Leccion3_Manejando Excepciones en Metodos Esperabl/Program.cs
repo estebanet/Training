d6a81c104fcf26ef4b8d4e5f1e6ba3c45b07224e
estebanet <38247804+estebanet@users.noreply.github.com>
2018-04-24 22:34:43 -0500

    EGR
    Se añade lección 3, Manejando excepciones en métodos esperables.

---
+++ TaskParallelLibrary-Training/Leccion3_Manejando Excepciones en Metodos Esperabl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leccion3_Manejando_Excepciones_en_Metodos_Esperabl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TaskParallelLibrary-Training/Leccion3_Manejando excepciones en Awaitable Method/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Leccion3_Manejando_excepciones_en_Awaitable_Method
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Continuar con excepciones no observadas - Lección 3 - Manejando excepciones en métodos esperables.
+        }
+
+        private async void GetWebContent_Click(object sender, RoutedEventArgs e)
+        {
+            using (WebClient webClientObj = new WebClient())
+            {
+                Task<string> result = null;
+                try
+                {
+
+                    //string resultado = await Task.Run<string>(() => { Thread.Sleep(3000); throw new Exception("Fail"); return "Hola :)"; });
+
+                    //result = webClientObj.DownloadStringTaskAsync("http://ticapacitacion.com2");
+
+                    //var contentString = await result;
+
+                    var resultadoT = GetStringAsync();
+
+                    Result.Content = await resultadoT;
+
+                    //var content = result.Result; No funciona
+
+                    //Task.WaitAll(result); No funciona
+
+                }
+                catch (WebException we)
+                {
+                    Result.Content = $"Se ha generado una excepción: {we.Message}, " +
+                        $"Estatus de Task: {result?.Status}";
+                }
+                catch (AggregateException ae)
+                {
+                    Result.Content = "Excepción manejada de tipo AggregateException";
+                }
+                catch (Exception ex)
+                {
+                    Result.Content = $"Excepción manejada de tipo Exception; Error: {ex.Message}";
+                }
+            }
+        }
+
+        public async Task<string> GetStringAsync()
+        {
+            string resultado = string.Empty;
+
+            var t1= Task.Run(() => { Thread.Sleep(3000); throw new Exception("Fail"); return "Hola :)"; });
+            var t2 = Task.Run(() => { Thread.Sleep(1000); throw new Exception("Fail 2"); return "Hola :)"; });
+
+            //resultado = await t1;
+
+            resultado += await t2;
+
+            resultado = await t1;
+
+            return resultado;
+        }
+    }
+}
