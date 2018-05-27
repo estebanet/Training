43f683f64b67f7385d63c4870204bb6ffc83e52f
Esteban GaRo <estebangaro@outlook.com>
2018-05-27 13:11:38 -0500

    EGR 2018/05/27 - Ae añade proyecto de clientes asp to azure functions

---
+++ AzureFunctionClients-Training/httpTriggeredFunction_ASPClient/Scripts/JavaScript/jQuery/azure_function_client.js
@@ -0,0 +1,35 @@
+﻿$(function () {
+    $('#btnGET').click(function () {
+        $.ajax({
+            method: 'GET',
+            url: 'https://function-app-tests-garo.azurewebsites.net/api/HttpTriggerCSharp1?code=yjKcDjiz9HfcT54aHO36vqUNF8LnRqhzzbUzq8lAkIS3Fd1XCH600A==' +
+                '&nombre=' + $('#nameGET').val() + '&apellido=' + $('#apellidoGET').val(),
+            dataType: 'json',
+            success: function (data) {
+                alert('Respuesta recibida (GET): ' + data.Message);
+            },
+            error: function () {
+                alert('Ha fallado la solicitud (GET) hacía la azure function');
+            }
+        });
+    });
+
+    $('#btnPOST').click(function () {
+        $.ajax({
+            method: 'POST',
+            url: 'https://function-app-tests-garo.azurewebsites.net/api/HttpTriggerCSharp1?code=yjKcDjiz9HfcT54aHO36vqUNF8LnRqhzzbUzq8lAkIS3Fd1XCH600A==',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify({
+                'nombre': $('#namePOST').val(),
+                'apellido': $('#apellidoPOST').val()
+            }),
+            success: function (data) {
+                alert('Respuesta recibida (POST): ' + data.Message);
+            },
+            error: function () {
+                alert('Ha fallado la solicitud (POST) hacía la azure function');
+            }
+        });
+    });
+});
