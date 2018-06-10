d02820aca6e2374be08f14181e20d96bca75f662
Esteban GaRo <estebangaro@outlook.com>
2018-06-10 00:24:42 -0500

    Enrutamiento por atributos
    Se añade ejercicio práctico, sobre el enrutamiento basado por atributos de enrutamiento.

---
+++ WebApi-EnMetodosHttp/WebApi-EnNomAcciones/Controllers/ColorBase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi_EnMetodosHttp.Models;
+
+namespace WebApi_EnNomAcciones.Controllers
+{
+    public class ColorBase: ApiController
+    {
+        public virtual async Task<IHttpActionResult> GetPorId(int identifer)
+        {
+            WebApi_EnMetodosHttp.Models.ColorsActionsLogic Logic =
+                new ColorsActionsLogic();
+            Color color = await Logic.GetColorById(identifer);
+
+            if (color == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(color);
+        }
+        
+        public virtual async Task<IHttpActionResult> SaveColor(Color color)
+        {
+            WebApi_EnMetodosHttp.Models.ColorsActionsLogic Logic =
+                new ColorsActionsLogic();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            color = await Logic.PostColor(color);
+
+            return CreatedAtRoute("DefaultApi", new { id = color.Id }, color);
+        }
+        
+        public virtual async Task<IHttpActionResult>
+            Actualizar(int id, Color color)
+        {
+            WebApi_EnMetodosHttp.Models.ColorsActionsLogic Logic =
+                new ColorsActionsLogic();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != color.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await Logic.UpdateColor(color);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await Logic.ColorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        public virtual async Task<IHttpActionResult> Eliminar(int identifer)
+        {
+            WebApi_EnMetodosHttp.Models.ColorsActionsLogic Logic =
+                new ColorsActionsLogic();
+
+            Color color = await Logic.DeleteColor(identifer);
+
+            if (color == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(color);
+        }
+    }
+}
