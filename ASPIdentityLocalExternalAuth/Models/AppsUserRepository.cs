4c4247c9d7adda6294de044b1c301e4186ae2f64
esteban_net_vm <38247804+estebanet@users.noreply.github.com>
2018-08-14 18:54:01 -0500

    Se actualiza ejercicio identity

---
+++ ASPIdentityLocalExternalAuth/Models/AppsUserRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AppsUserRepository
+    {
+        public async Task<EntityFramework.User> FindUserAsync(string userIdentifier,
+            bool getInfo = false)
+        {
+            EntityFramework.User User;
+
+            try
+            {
+                using (EntityFramework.WebApp1EFMContainer DbContex =
+                    new EntityFramework.WebApp1EFMContainer())
+                {
+                    User = await DbContex.Users.FindAsync(userIdentifier);
+                    if (User == null)
+                    {
+                        User = new EntityFramework.User();
+                    }
+
+                    if (getInfo)
+                    {
+                        //Para forzar la recuperación de entidades de propiedades de navegación.
+                        string TempString =
+                            User.Profile.UserIdentifier.ToString();
+                        TempString = string.Join(",", User.Roles.Select(Rol => Rol.Name));
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                User = null;
+                System.Diagnostics.Debug.WriteLine("Se ha generado una excepción durante la ejecución del método FindUserAsync;" +
+                    $"Error: {ex.Message}");
+            }
+
+            return User;
+        }
+
+        public async Task<bool> ValidateUserAsync(string userIdentifier, string password)
+        {
+            var User = await FindUserAsync(userIdentifier);
+
+            return string.Equals(User?.Password, password);
+        }
+    }
+}
---
+++ ASPIdentityLocalExternalAuth/Models/EntityFramework/WebApp1EFM.edmx.sql
@@ -0,0 +1,145 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 07/20/2018 14:10:52
+-- Generated from EDMX file: C:\Users\estebangaro\Documents\GitHub\Training\ASPIdentityLocalExternalAuth\Models\EntityFramework\WebApp1EFM.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [garo-bd-test-1];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_UserProfile]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Profiles] DROP CONSTRAINT [FK_UserProfile];
+GO
+IF OBJECT_ID(N'[dbo].[FK_UserRole]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Users] DROP CONSTRAINT [FK_UserRole];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Users]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Users];
+GO
+IF OBJECT_ID(N'[dbo].[Profiles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Profiles];
+GO
+IF OBJECT_ID(N'[dbo].[Roles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Roles];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [UserIdentifier] nvarchar(60)  NOT NULL,
+    [Password] nvarchar(255)  NOT NULL
+);
+GO
+
+-- Creating table 'Profiles'
+CREATE TABLE [dbo].[Profiles] (
+    [UserIdentifier] nvarchar(60)  NOT NULL,
+    [Name] nvarchar(60)  NULL,
+    [FirstName] nvarchar(60)  NULL,
+    [LastName] nvarchar(60)  NULL,
+    [Address] nvarchar(255)  NULL,
+    [DateOfBirth] datetime  NULL
+);
+GO
+
+-- Creating table 'Roles'
+CREATE TABLE [dbo].[Roles] (
+    [Id] smallint IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(60)  NOT NULL,
+    [Description] nvarchar(120)  NULL
+);
+GO
+
+-- Creating table 'RoleUser'
+CREATE TABLE [dbo].[RoleUser] (
+    [Roles_Id] smallint  NOT NULL,
+    [Users_UserIdentifier] nvarchar(60)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [UserIdentifier] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([UserIdentifier] ASC);
+GO
+
+-- Creating primary key on [UserIdentifier] in table 'Profiles'
+ALTER TABLE [dbo].[Profiles]
+ADD CONSTRAINT [PK_Profiles]
+    PRIMARY KEY CLUSTERED ([UserIdentifier] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Roles'
+ALTER TABLE [dbo].[Roles]
+ADD CONSTRAINT [PK_Roles]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Roles_Id], [Users_UserIdentifier] in table 'RoleUser'
+ALTER TABLE [dbo].[RoleUser]
+ADD CONSTRAINT [PK_RoleUser]
+    PRIMARY KEY CLUSTERED ([Roles_Id], [Users_UserIdentifier] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [UserIdentifier] in table 'Profiles'
+ALTER TABLE [dbo].[Profiles]
+ADD CONSTRAINT [FK_UserProfile]
+    FOREIGN KEY ([UserIdentifier])
+    REFERENCES [dbo].[Users]
+        ([UserIdentifier])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Roles_Id] in table 'RoleUser'
+ALTER TABLE [dbo].[RoleUser]
+ADD CONSTRAINT [FK_RoleUser_Role]
+    FOREIGN KEY ([Roles_Id])
+    REFERENCES [dbo].[Roles]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Users_UserIdentifier] in table 'RoleUser'
+ALTER TABLE [dbo].[RoleUser]
+ADD CONSTRAINT [FK_RoleUser_User]
+    FOREIGN KEY ([Users_UserIdentifier])
+    REFERENCES [dbo].[Users]
+        ([UserIdentifier])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_RoleUser_User'
+CREATE INDEX [IX_FK_RoleUser_User]
+ON [dbo].[RoleUser]
+    ([Users_UserIdentifier]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
