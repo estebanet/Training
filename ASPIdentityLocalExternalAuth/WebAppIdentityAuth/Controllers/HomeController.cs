d963861e152c8dd0f9ba0587214c3c0ca622a079
esteban_net_vm <38247804+estebanet@users.noreply.github.com>
2018-08-15 16:54:51 -0500

    Se actualiza ejercicio de ASP NET Identity

---
+++ ASPIdentityLocalExternalAuth/WebAppIdentityAuth/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppIdentityAuth.Controllers
+{
+    [Models.Filters.Auth(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ClaimsPrincipal Principal =
+                //System.Threading.Thread.CurrentPrincipal as ClaimsPrincipal;
+                //HttpContext.User as ClaimsPrincipal;
+                this.User as ClaimsPrincipal;
+
+            return View();
+        }
+
+        public ActionResult AuthenticatedUsers()
+        {
+            return View();
+        }
+    }
+}
