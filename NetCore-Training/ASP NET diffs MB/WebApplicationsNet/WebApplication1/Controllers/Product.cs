67840ff0fd7992cc0f52b2101df2d5004d488663
estebanet <38247804+estebanet@users.noreply.github.com>
2018-10-13 01:26:18 -0500

    Se incluye implementación de api en net core

--- NetCore-Training/ASP NET diffs MB/WebApplicationsNet/WebApplication1/Controllers/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Controllers
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int VendorId { get; set; }
-    }
-}
---
+++ NetCore-Training/NetCoreTests/WebApplication1_api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1_api.Models
+{
+    public class User
+    {
+        public User() { }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
